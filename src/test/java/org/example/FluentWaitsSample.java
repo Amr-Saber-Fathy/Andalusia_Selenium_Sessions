44bfb80ec907ea6fbaa86878a950e8bccfbccd5c
Amr <amr.saber.fathy@gmail.com>
2026-02-06 22:37:10 +0200

    Updating FluentWaitsSample

--- src/test/java/org/example/FluentWaitsSample.java
+++ src/test/java/org/example/FluentWaitsSample.java
@@ -1,17 +1,15 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class WaitsSample {
+public class FluentWaitsSample {
 
     static String firstName = "Amr";
     static String lastName = "Saber";
@@ -22,12 +20,10 @@ public class WaitsSample {
 
         WebDriver driver = new ChromeDriver();
 
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        Wait<WebDriver> wait = new FluentWait<>(driver)
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(10))
+                .ignoring(NoSuchElementException.class)
                 .pollingEvery(Duration.ofSeconds(2));
 
         driver.get("https://qacart-todo.herokuapp.com/signup");
@@ -35,13 +31,9 @@ public class WaitsSample {
 
         //first name field
 
-//        Thread.sleep(10000);
         By firstNameFieldLocator = By.cssSelector("input[data-testid='first-name2']");
-//        WebElement firstNameField = wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameFieldLocator));
 
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[data-testid='first-name2']"))).sendKeys(firstName);
-//        WebElement firstNameField = driver.findElement(firstNameFieldLocator);
-        WebElement firstNameField = wait.until(driver2 -> driver2.findElement(firstNameFieldLocator));
+        WebElement firstNameField = wait.until(d -> d.findElement(firstNameFieldLocator));
 
         firstNameField.sendKeys(firstName);
 
